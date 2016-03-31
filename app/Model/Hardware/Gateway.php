a78f76899712b629ad107da6c050910f079f158a
Sam Chen <xuejian1354@gmail.com>
2016-03-31 11:59:53 +0800

    WebSocket JS: Changing something for adapt websocket using by node.js

--- app/Model/Hardware/Gateway.php
+++ app/Model/Hardware/Gateway.php
@@ -8,5 +8,5 @@ class Gateway extends Model
 {
     protected $table = 'gateways';
 
-    protected $fillable = ['name', 'gw_sn', 'transtocol', 'ip', 'udp_port', 'tcp_port', 'http_url', 'area', 'ispublic', 'owner'];
+    protected $fillable = ['name', 'gw_sn', 'transtocol', 'ip', 'udp_port', 'tcp_port', 'http_url', 'ws_url', 'area', 'ispublic', 'owner'];
 }
